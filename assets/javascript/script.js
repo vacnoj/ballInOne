cc82762a990e6b48f8bf64208e62c5d241c600de
Jon Caviness <jcaviness@10xpeople.com>
2019-09-04 16:56:55 -0600

    updated

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -136,6 +136,31 @@ $('#zipcode').hide();
     $('#allInventory').append(response);
   });
 
-
+  function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+      }
+    };
+    xhttp.open("GET", "assets/xml/allInventory.xml", true);
+    xhttp.send();
+  }
+  function myFunction(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table="<tr><th>Name</th><th>Size</th></tr>";
+    var x = xmlDoc.getElementsByTagName("BALL");
+    for (i = 0; i <x.length; i++) { 
+      table += "<tr><td>" +
+      x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("SIZE")[0].childNodes[0].nodeValue +
+      "</td></tr>";
+    }
+    document.getElementById("allInventory").innerHTML = table;
+  }
+  
+  loadDoc();
 
 });
